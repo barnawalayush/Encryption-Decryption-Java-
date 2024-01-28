ec144644f9698756c5f6a2069db9e4528a00470b
Ayush <abarnawal@griddynaamics.com>
2024-01-28 16:48:00 +0530

    stage_2 complete

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -1,20 +1,39 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
 
     final static String mapping = "zyxwvutsrqponmlkjihgfedcba";
 
     public static void main(String[] args) {
 
-        String original_text = "we found a treasure!";
+        Scanner sc = new Scanner(System.in);
 
-        String encrypted_text = encrypt(original_text);
+        String original_text = sc.nextLine();
+        int shift = sc.nextInt();
+
+        String encrypted_text = encryptViaShift(original_text, shift);
+        //String encrypted_text = encrypt(original_text);
 
         System.out.println(encrypted_text);
 
     }
 
-    public static String encrypt(String original_text){
+    private static String encryptViaShift(String originalText, int key) {
+        StringBuilder encrypted_text = new StringBuilder("");
+
+        for(int i=0; i<originalText.length(); i++){
+            char ch = originalText.charAt(i);
+            if(ch>='a' && ch<='z')
+                encrypted_text.append((char)('a' + ((originalText.charAt(i) - 'a' + key) % 26)));
+            else encrypted_text.append(" ");
+        }
+
+        return encrypted_text.toString();
+    }
+
+    public static String encrypt(String original_text){   // a->z, b->y, c->x....
         String encrypted_text = "";
 
         for(int i=0; i<original_text.length(); i++){
